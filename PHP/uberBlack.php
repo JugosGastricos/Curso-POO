277c52859fd3e90fe089038ff5b6aac0a61c344c
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2022-07-05 22:03:37 -0500

    Clase 26: Se aplicó la herencia en Java y PHP, creando cada uno de los servicios de uber como subclases de Car

---
+++ PHP/uberBlack.php
@@ -0,0 +1,12 @@
+<?php
+class UberBlack extends Car {
+    public $typeCarAccepted = array();
+    public $seatsMaterial = array();
+
+    public function __construct($license, $driver, $typeCarAccepted, $seatsMaterial){
+        parent::__construct($license, $driver);
+        $this->typeCarAccepted = $typeCarAccepted;
+        $this->seatsMaterial = $seatsMaterial;
+    }
+}
+?>
