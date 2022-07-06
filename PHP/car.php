5d24b267f40594cccf640fc16fa06ccaa9ee1d94
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2022-07-05 21:15:55 -0500

    Clase 25: se realizaron los constructores de las clases Account y Car en PHP con la ayuda de los comentarios de la comunidad

--- PHP/car.php
+++ PHP/car.php
@@ -4,5 +4,14 @@ class Car {
     public $license;
     public $driver;
     public $passenger;
+
+    public function __construct($license, $driver){
+        $this->driver = $driver;
+        $this->license = $license;
+    }
+
+    public function printDataCar(){
+        echo "License: $this->license, Driver: {$this->driver->name}, Document: {$this->driver->document}";
+    }
 }
 ?>
--- PHP/main.php
+++ PHP/main.php
@@ -1,3 +1,8 @@
 <?php
-echo 'Hola mundo';
+// echo 'Hola mundo';
+require_once('car.php');
+require_once('account.php');
+
+$car = new Car("AMG063", new Account("Mercedes Campuzano", "LOL420"));
+$car->printDataCar();
 ?>
