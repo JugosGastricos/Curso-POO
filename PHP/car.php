4e1ff6b286cf668f18ca55a71bfad0b6dd432dea
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2022-07-08 14:32:24 -0500

    Clase 32: Se realizó el encapsulamiento con getters y setters en PHP

--- PHP/car.php
+++ PHP/car.php
@@ -3,15 +3,32 @@ class Car {
     public $id;
     public $license;
     public $driver;
-    public $passenger;
-
+    private $passenger;
+    
     public function __construct($license, $driver){
         $this->driver = $driver;
         $this->license = $license;
     }
 
+
+    public function getPassengers(){
+        return $this->passenger;
+    }
+
+    public function setPassengers($passengers){
+        if($passengers == 4){
+            $this->passenger = $passengers;
+        } else {
+            $this->passenger = "undefined";
+            throw new Error('El número de pasajeros debe ser de 4');
+        }
+    }
+
     public function printDataCar($brand, $model){
-        echo "License: $this->license, Driver: {$this->driver->name}, Document: {$this->driver->document}, Brand: $brand, Model: $model </br>";
+        if ($brand == "" || $model == "" || $this->passenger == ""){
+            throw new Error('Ingresa todos los datos necesarios');
+        } else {
+            echo "License: $this->license <br/>Driver: {$this->driver->name} <br/>Document: {$this->driver->document} <br/>Brand: $brand <br/>Model: $model </br>Passengers: $this->passenger</br><br/>";
+        }
     }
 }
-?>
--- PHP/index.php
+++ PHP/index.php
@@ -4,12 +4,14 @@ require_once('car.php');
 require_once('account.php');
 require_once('uberX.php');
 require_once('uberPool.php');
+require_once('driver.php');
 
 // $car = new Car("AMG063", new Account("Mercedes Campuzano", "LOL420"));
 // $car->printDataCar();
-$uberX = new UberX("AMG063", new Account("Mercedes Campuzano", "LOL420"), "Chevrolet", "Spark");
+$uberX = new UberX("AMG063", new Driver("Mercedes Campuzano", "LOL420"), "Chevrolet", "Spark");
+$uberX->setPassengers(4);
 $uberX->printDataCar($uberX->brand, $uberX->model);
 
-$uberPool = new UberPool("FUK069", new Account("Andrea Serna", "69420666"), "Volkswagen", "Jetta");
+$uberPool = new UberPool("FUK069", new Driver("Andrea Serna", "69420666"), "Volkswagen", "Jetta");
+$uberPool->setPassengers(4);
 $uberPool->printDataCar($uberPool->brand, $uberPool->model);
-?>
